218456548317074687a11149e54fd4842a9b4140
madhubandru <madhubandru@gmail.com>
2020-07-26 23:20:56 -0400

    update flatten_json version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     #packages=["dxc", "dxc.ai"],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json","pyjanitor","ftfy","arrow",
+    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow",
                       "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community","flask_cors","gevent"],
     entry_points={
         "console_scripts": [
