1a23e65661febfc9a02dafb44674e05fc0f493f0
Likhil <likhilrachuri@gmail.com>
2020-09-17 14:21:39 -0400

    two elements are added into the notebook

--- dxc/ai/clean_data/clean_data.py
+++ dxc/ai/clean_data/clean_data.py
@@ -7,27 +7,10 @@ import scrubadub #data cleaning
 import arrow #normalizing dates
 import numpy as np
 from sklearn.base import TransformerMixin
-from fancyimpute import KNN
-from sklearn.preprocessing import OrdinalEncoder
+from fancyimpute import KNN ##using KNN as imputer for categorical fields
+from sklearn.preprocessing import OrdinalEncoder ##Ordinal encoder is being used for encoding categorical objects
 from dxc.ai.global_variables import globals_file
 
-
-class DataFrameImputer(TransformerMixin):
-    def __init__(self):
-        """Impute missing values.
-        Columns of dtype object are imputed with the most frequent value 
-        in column.
-        Columns of other types are imputed with mean of column.
-        """
-    def fit(self, X, y=None):
-        self.fill = pd.Series([X[c].value_counts().index[0]
-            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
-            index=X.columns)
-
-        return self
-
-    def transform(self, X, y=None):
-        return X.fillna(self.fill)
 
 def encode(data):
 
@@ -88,7 +71,7 @@ def clean_dataframe(df, impute = False, text_fields = [], date_fields = [], nume
   
     #impute missing values
     if impute:
-        clean_df = DataFrameImputer().fit_transform(clean_df)
+        clean_df = impute(clean_df)
 
     #standardize the format of all date fields
     for field in date_fields:
