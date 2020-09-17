47f3bfa95798ca0b5cefc34c30225a640267b6be
Kishor Pulagam <pulagamnkr@gmail.com>
2020-09-17 15:56:42 -0400

    Added Visualization report
    Added complete visualization report of raw data

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="DXC-Industrialized-AI-Starter",
-    version="2.0.7",
+    version="2.1.1",
     description="Python library which is extensively used for all AI projects",
     long_description=readme(),
     long_description_content_type="text/markdown",
@@ -24,7 +24,7 @@ setup(
     #packages=["dxc", "dxc.ai"],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow",
+    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow","pandas-profiling[notebook]==2.9.0",
                       "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community==0.14.1","flask_cors","gevent"],
     entry_points={
         "console_scripts": [
