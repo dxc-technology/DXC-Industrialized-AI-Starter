c8bc8a6fd8f7addc132db1e355d93ba1a8c541d4
Karthikreddy Kuna <55387103+karthikreddykuna@users.noreply.github.com>
2020-09-30 11:18:15 -0400

    Tpot Auto_ml integration

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
 #     include_package_data=True,
     install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow","pandas-profiling[notebook]==2.9.0",
-                      "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community==0.14.1","flask_cors","gevent"],
+                      "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community==0.14.1","flask_cors","gevent","tpot","feature_engine"],
     entry_points={
         "console_scripts": [
             "dxc=dxc.ai:main",
