699754aa0339f65ccd7076a3fba53e9eb467d8ba
Karthikreddy Kuna <55387103+karthikreddykuna@users.noreply.github.com>
2020-09-30 11:21:59 -0400

    Tpot Auto_ml integration

--- dxc/ai/global_variables/globals_file.py
+++ dxc/ai/global_variables/globals_file.py
@@ -1,3 +1,12 @@
 clean_data_used = False
 wrt_raw_data_used = False
 run_experiment_used = False
+#below global variables are related to warm_start feature of auto_ml. Please don't change them. 
+run_experiment_warm_start = False
+run_experiment_x_train = None
+run_experiment_x_test = None
+run_experiment_y_train = None
+run_experiment_y_test = None
+run_experiment_model = None
+run_experiment_datatransformed = None
+run_experiment_labeldata = None
