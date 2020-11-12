5a2e2a247e913518682636bfd1824a9bc27ff42d
Karthikreddy Kuna <55387103+karthikreddykuna@users.noreply.github.com>
2020-11-12 09:04:29 -0500

    Autoencoder Changes for publish microservice

--- dxc/ai/publish_microservice/publish_microservice.py
+++ dxc/ai/publish_microservice/publish_microservice.py
@@ -64,6 +64,23 @@ def publish_microservice(microservice_design, trained_model, verbose = False):
     api_model = "{}/{}".format(microservice_design["model_path"], microservice_design["microservice_name"])
     client.file(api_model).putFile(local_model)
 
+    if globals_file.run_experiment_encoder_used:
+        encode_model = 'encode_file.pkl'
+        encode_output = open(encode_model, 'wb')
+        pickle.dump(globals_file.run_experiment_encoder, encode_output)
+        encode_output.close()
+        encode_folder =  microservice_design["microservice_name"] + '_encoder'
+        encode_path = "{}/{}".format(microservice_design["model_path"], encode_folder)
+        client.file(encode_path).putFile(encode_model)
+    if globals_file.run_experiment_target_encoder_used:
+        target_encode_model = 'target_encode_file.pkl'
+        target_encode_output = open(target_encode_model, 'wb')
+        pickle.dump(globals_file.run_experiment_target_encoder, target_encode_output)
+        target_encode_output.close()
+        target_encode_folder =  microservice_design["microservice_name"] + '_target_encoder'
+        target_encode_path = "{}/{}".format(microservice_design["model_path"], target_encode_folder)
+        client.file(target_encode_path).putFile(target_encode_model)
+
     # encode API key, so we can use it in the git URL
     encoded_api_key = urllib.parse.quote_plus(microservice_design["api_key"])
 
@@ -101,6 +118,10 @@ def publish_microservice(microservice_design, trained_model, verbose = False):
     # defines the source for the microservice
     results = "{'results':prediction}"
     file_path = "'" + api_model + "'"
+    if globals_file.run_experiment_encoder_used:
+        encodefile_path = "'" + encode_path + "'"
+    if globals_file.run_experiment_target_encoder_used:
+        target_encodefile_path = "'" + target_encode_path + "'"
     ##Don't change the structure of below docstring
     ##this is the source code needed for the microservice
     
@@ -155,8 +176,121 @@ def apply(input):
 \tprediction = json.dumps(prediction, default=default)
 \treturn {results}"""
 
+    ## source code for generalized tpot model
+    src_code_generalized_encode = """import Algorithmia
+import auto_ml
+import pandas as pd
+import pickle
+import json
+import numpy as np
+import feature_engine
+# create an Algorithmia client
+client = Algorithmia.client()
+def load_model():
+    # Get file by name
+    # Open file and load model
+\tfile_path = {file_path}
+\tmodel_path = client.file(file_path).getFile().name
+    # Open file and load model
+\twith open(model_path, 'rb') as f:
+\t\tmodel = pickle.load(f)
+\t\treturn model
+trained_model = load_model()
+def load_encode():
+    # Get file by name
+    # Open file and load encoder
+\tencodefile_path = {encodefile_path}
+\tencode_path = client.file(encodefile_path).getFile().name
+    # Open file and load encoder
+\twith open(encode_path, 'rb') as f:
+\t\tencoder = pickle.load(f)
+\t\treturn encoder
+encode = load_encode()
+def default(obj):
+    if type(obj).__module__ == np.__name__:
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return obj.item()
+    raise TypeError('Unknown type:', type(obj))
+def apply(input):
+\tinput = pd.DataFrame([input])
+\ttry:
+\t\tinput = encode.transform(input)
+\texcept:
+\t\tpass
+\tprediction = trained_model.predict(input)
+\tprediction = json.dumps(prediction[0], default=default)
+\treturn {results}"""
+
+    ## source code for generalized tpot model
+    src_code_generalized_target_encode = """import Algorithmia
+import auto_ml
+import pandas as pd
+import pickle
+import json
+import numpy as np
+import feature_engine
+# create an Algorithmia client
+client = Algorithmia.client()
+def load_model():
+    # Get file by name
+    # Open file and load model
+\tfile_path = {file_path}
+\tmodel_path = client.file(file_path).getFile().name
+    # Open file and load model
+\twith open(model_path, 'rb') as f:
+\t\tmodel = pickle.load(f)
+\t\treturn model
+trained_model = load_model()
+def load_encode():
+    # Get file by name
+    # Open file and load encoder
+\tencodefile_path = {encodefile_path}
+\tencode_path = client.file(encodefile_path).getFile().name
+    # Open file and load encoder
+\twith open(encode_path, 'rb') as f:
+\t\tencoder = pickle.load(f)
+\t\treturn encoder
+encode = load_encode()
+def load_target_encode():
+    # Get file by name
+    # Open file and load target encoder
+\ttarget_encodefile_path = {target_encodefile_path}
+\ttarget_encode_path = client.file(target_encodefile_path).getFile().name
+    # Open file and load target encoder
+\twith open(target_encode_path, 'rb') as f:
+\t\ttarget_encoder = pickle.load(f)
+\t\treturn target_encoder
+target_encode = load_target_encode()
+
+def default(obj):
+    if type(obj).__module__ == np.__name__:
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return obj.item()
+    raise TypeError('Unknown type:', type(obj))
+def apply(input):
+\tinput = pd.DataFrame([input])
+\ttry:
+\t\tinput = encode.transform(input)
+\texcept:
+\t\tpass
+\tprediction = trained_model.predict(input)
+\ttry:
+\t\tprediction = target_encode.inverse_transform(prediction)
+\t\tprediction = prediction[0]
+\texcept:
+\t\tprediction = json.dumps(prediction[0], default=default)
+\treturn {results}"""
+
     if globals_file.run_experiment_used:
         src_code_content = src_code_generalized
+        if globals_file.run_experiment_encoder_used:
+            src_code_content = src_code_generalized_encode
+        if globals_file.run_experiment_target_encoder_used:
+            src_code_content = src_code_generalized_target_encode       
 
     splitted=src_code_content.split('\n')
 
@@ -165,6 +299,10 @@ def apply(input):
         for line in splitted:
             if line.strip()=="file_path = {file_path}":
                 line="\tfile_path = {}".format(file_path)
+            if line.strip()=="encodefile_path = {encodefile_path}":
+                line="\tencodefile_path = {}".format(encodefile_path)
+            if line.strip()=="target_encodefile_path = {target_encodefile_path}":
+                line="\ttarget_encodefile_path = {}".format(target_encodefile_path)
             if line.strip()=="return {results}":
                 line="\treturn {}".format(results)
             f.write(line + '\n')
@@ -176,6 +314,7 @@ def apply(input):
     auto_ml
     pandas
     numpy
+    feature-engine
     bottleneck==1.2.1"""
 
     post_split=requirements_file_content.split('\n')
