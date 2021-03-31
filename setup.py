7cb46128afb9d5406ede57247715d6d94e517ac9
Karthikreddy Kuna <55387103+karthikreddykuna@users.noreply.github.com>
2021-03-30 22:52:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     #packages=["dxc", "dxc.ai"],
     packages=find_packages(),
 #     include_package_data=True,
-    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow","pandas-profiling[notebook]==2.9.0",
+    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia==1.7.10","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow","pandas-profiling[notebook]==2.9.0",
                       "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community","flask_cors","gevent","tpot","feature_engine",
                      "tensorflow","ktrain","raiwidgets"],
     entry_points={
