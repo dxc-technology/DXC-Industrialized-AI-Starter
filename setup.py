b5c56bc1015d61cf1a627f1b82539d3f7b902887
Karthikreddy Kuna <55387103+karthikreddykuna@users.noreply.github.com>
2021-08-18 10:51:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
     #packages=["dxc", "dxc.ai"],
     packages=find_packages(),
 #     include_package_data=True,
-    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia==1.7.8","gitpython","flatten_json==0.1.13","pyjanitor","ftfy","arrow","pandas-profiling[notebook]==2.9.0",
-                      "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community","flask_cors","gevent","tpot","feature_engine","tensorflow","ktrain","raiwidgets","pandas","janitor", "gym==0.18.0","keras-rl2","tensorflow"],
+    install_requires=["JIRA>=3.0.1","scikit-learn>=0.24.2","Algorithmia>=1.10.0","gitpython","flatten_json==0.1.13","pyjanitor","ftfy","arrow","pandas-profiling[notebook]==2.9.0",
+                      "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community","flask_cors","gevent","tpot","feature_engine","tensorflow","ktrain>=0.27.2","raiwidgets","pandas","janitor", "gym==0.18.0","keras-rl2>=1.0.5"],
     entry_points={
         "console_scripts": [
             "dxc=dxc.ai:main",
