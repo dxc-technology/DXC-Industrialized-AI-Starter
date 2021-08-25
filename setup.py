5745aacf52ec28bb9620a2a74b22f28742e78afa
Karthikreddy Kuna <55387103+karthikreddykuna@users.noreply.github.com>
2021-08-24 22:38:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     #packages=["dxc", "dxc.ai"],
     packages=find_packages(),
 #     include_package_data=True,
-    install_requires=read_requirements(),
+    install_requires=[line.strip() for line in open("requirements.txt").readlines()],
     entry_points={
         "console_scripts": [
             "dxc=dxc.ai:main",
