6b0db26bea2997d7dc0a189a63ac9b88d0faa338
Madhu Bandru <63613662+madhubandru@users.noreply.github.com>
2021-11-03 10:07:31 -0500

    Add run_experiment test case

--- Tests/Automate Test cases/test_ai.py
+++ Tests/Automate Test cases/test_ai.py
@@ -111,3 +111,19 @@ def test_datepipeline():
     df = ai.access_data_from_pipeline(wrt_raw_data, data_pipeline())
     assert type(df) == type(pd.DataFrame())
     assert loaded_data.empty == False
+    
+    
+def test_experiment():
+    global trained_model
+    try:
+        experiment_design = {
+            #model options include ['tpot_regression()', 'tpot_classification()']
+            "model": ai.tpot_regression(),
+            "labels": df.avg_est_unit_cost_error,
+            "data": df,
+        }
+
+        trained_model = ai.run_experiment(experiment_design, verbose = False, max_time_mins = 5, max_eval_time_mins = 0.04, 
+                                          config_dict = None, warm_start = False, export_pipeline = True, scoring = None)
+    except:
+        print('----------MODEL BUILDING FAILED----------')
