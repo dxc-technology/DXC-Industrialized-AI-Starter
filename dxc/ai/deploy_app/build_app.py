c6f4394ec3ce5bdbe4469b46bff1972fe98e277c
Kishor Pulagam <pulagamnkr@gmail.com>
2022-01-18 20:38:56 -0600

    Update build_app.py

--- dxc/ai/deploy_app/build_app.py
+++ dxc/ai/deploy_app/build_app.py
@@ -21,12 +21,12 @@ def generate_req_files():
 
 def generate_app_script(data_path='', encoder_path='', target_encoder_path='', app_title='App deployed by AI-Starter'):    
     requirements = """pip-upgrader
-    pandas
-    requests
-    pickle-mixin
-    scikit-learn
-    streamlit
-    feature-engine
+pandas
+requests
+pickle-mixin
+scikit-learn
+streamlit
+feature-engine
     """
 
     with open('requirements.txt', 'w') as f:
@@ -45,73 +45,73 @@ def generate_app_script(data_path='', encoder_path='', target_encoder_path='', a
 
     #code of app.py
     app_script = """import streamlit as st
-    import pickle
-    from io import BytesIO
-    import requests
-    import pandas as pd 
+import pickle
+from io import BytesIO
+import requests
+import pandas as pd 
 
 
-    # Code from Best Pipeline.py here
+# Code from Best Pipeline.py here
 
-    best_pipeline
+best_pipeline
 
-    ######################
+######################
 
-    # User defined values
+# User defined values
 
-    title = 'title of the app'
-    encoder_location = 'encoder.pkl'
-    target_encoder_location = 'target_encode.pkl'
+title = 'title of the app'
+encoder_location = 'encoder.pkl'
+target_encoder_location = 'target_encode.pkl'
 
-    if len(encoder_location) > 5:
-        mfile = BytesIO(requests.get(encoder_location).content)
-        encoder = pickle.load(mfile)
-        df = encoder.inverse_transform(features)
-    else:
-        df = features.copy()
+if len(encoder_location) > 5:
+    mfile = BytesIO(requests.get(encoder_location).content)
+    encoder = pickle.load(mfile)
+    df = encoder.inverse_transform(features)
+else:
+    df = features.copy()
 
-    if len(target_encoder_location) > 5:
-        mfile = BytesIO(requests.get(target_encoder_location).content)
-        target_encoder = pickle.load(mfile)
+if len(target_encoder_location) > 5:
+    mfile = BytesIO(requests.get(target_encoder_location).content)
+    target_encoder = pickle.load(mfile)
 
-    st.title(title)
+st.title(title)
 
-    st.sidebar.header('User Input Parameters')
+st.sidebar.header('User Input Parameters')
 
-    st.subheader('User Input parameters')
+st.subheader('User Input parameters')
 
-    selected_data = dict()
-    for column in df.columns:
-        if column != 'target':
-            label = column.replace('_id.','')
-            label = label.replace('_',' ').title()
-            if df[column].dtype == 'O':
-                selected_value = st.sidebar.selectbox(label, list(df[column].unique()))
-            elif df[column].dtype == 'int64':
-                selected_value = st.sidebar.number_input(label, min_value=df[column].min(), max_value=df[column].max(), value=df[column].iloc[0], step=1)
+selected_data = dict()
+for column in df.columns:
+    if column != 'target':
+        label = column.replace('_id.','')
+        label = label.replace('_',' ').title()
+        if df[column].dtype == 'O':
+            selected_value = st.sidebar.selectbox(label, list(df[column].unique()))
+        elif df[column].dtype == 'int64':
+            selected_value = st.sidebar.number_input(label, min_value=df[column].min(), max_value=df[column].max(), value=df[column].iloc[0], step=1)
 
-            elif df[column].dtype == 'float64':
-                selected_value = st.sidebar.number_input(label, min_value=df[column].min(), max_value=df[column].max(), value=df[column].iloc[0])
-            
-            selected_data[column] = selected_value
+        elif df[column].dtype == 'float64':
+            selected_value = st.sidebar.number_input(label, min_value=df[column].min(), max_value=df[column].max(), value=df[column].iloc[0])
+        
+        selected_data[column] = selected_value
 
-    test_data = pd.DataFrame(selected_data, index=[0])
+test_data = pd.DataFrame(selected_data, index=[0])
 
-    st.write(test_data)
+st.write(test_data)
 
-    st.subheader('Prediction')
-    if len(encoder_location) > 5:
-        test_data = encoder.transform(test_data) 
+st.subheader('Prediction')
+if len(encoder_location) > 5:
+    test_data = encoder.transform(test_data) 
 
-    prediction = exported_pipeline.predict(test_data)
+prediction = exported_pipeline.predict(test_data)
 
-    if len(target_encoder_location) > 5:
-        prediction = target_encoder.inverse_transform(prediction)
+if len(target_encoder_location) > 5:
+    prediction = target_encoder.inverse_transform(prediction)
 
-    if 'float' in str(type(prediction[0])):
-        st.write(round(prediction[0],2))
-    else:
-        st.write(prediction[0])
+if 'float' in str(type(prediction[0])):
+    st.write(round(prediction[0],2))
+else:
+    st.write(prediction[0])
     """
 
     app_script = app_script.replace('best_pipeline', script)
